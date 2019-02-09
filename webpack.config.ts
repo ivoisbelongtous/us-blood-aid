cbd2fb2969818e8c18536d8d898d1a8920c984bd
Ivo Murrell <ivoisbelongtous@gmail.com>
2019-02-09 18:35:03 +0000

    Use plugin to generate HTML to serve bundle.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,13 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import * as path from "path";
 import { Configuration } from "webpack";
 
 const config: Configuration = {
   entry: "./src/index.ts",
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
   mode: "development",
   module: {
     rules: [
@@ -20,7 +24,8 @@ const config: Configuration = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({ title: "The Echo of the World" })]
 };
 
 module.exports = config;
