ac43fc206b53f6944d9e92f560eca355b725529f
Ivo Murrell <ivoisbelongtous@gmail.com>
2019-02-09 18:26:33 +0000

    Add retina support to HTML canvas output.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import * as d3 from "d3";
 import * as topojson from "topojson";
 import world from "world-atlas/world/110m.json";
 
-const context = d3
-  .select<HTMLCanvasElement, {}>("canvas")
-  .node()!
-  .getContext("2d")!;
+const width = 960;
+const height = 600;
+
+const canvas = d3
+  .select("body")
+  .append("canvas")
+  .attr("width", width * window.devicePixelRatio)
+  .attr("height", height * window.devicePixelRatio)
+  .style("width", width + "px")
+  .style("height", height + "px");
+const context = canvas.node()!.getContext("2d")!;
+context.scale(window.devicePixelRatio, window.devicePixelRatio);
+
 const path = d3.geoPath(d3.geoEqualEarth()).context(context);
 
 context.beginPath();
