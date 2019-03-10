8ac497111b63331ed70b0fddf54f3d20407c1be6
Ivo Murrell <ivoisbelongtous@gmail.com>
2019-03-10 15:29:30 +0000

    Use async/await pattern at top level.
    At some point I'll want to split the code into proper functions and will
    be able to eschew this wrapper, but this is preferable for now whilst
    the logic is strictly sequential.

--- src/index.ts
+++ src/index.ts
@@ -7,75 +7,76 @@ import world from "world-atlas/world/110m.json";
 
 import { parseAidRecord } from "parse";
 
-const width = 960;
-const height = 600;
+(async () => {
+  const width = 960;
+  const height = 600;
 
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-const features = topojson.feature(
-  world as any,
-  world.objects.countries as GeometryCollection
-).features;
+  const features = topojson.feature(
+    world as any,
+    world.objects.countries as GeometryCollection
+  ).features;
 
-const path = d3.geoPath(d3.geoEqualEarth());
+  const path = d3.geoPath(d3.geoEqualEarth());
 
-d3.csv(require("us_foreign_aid_country_deduped.csv"), parseAidRecord).then(
-  rows => {
-    const latestYear = rows.filter(
-      row =>
-        row.fiscalYear.getFullYear() === 2017 &&
-        row.countryName !== "World" &&
-        row.transactionTypeName === "Obligations"
-    );
-    const extent = d3.extent(latestYear, row =>
-      row.currentAmount >= 1 ? row.currentAmount : undefined
-    );
-    const colourScale = d3
-      // @ts-ignore
-      .scaleSequentialLog(d3.interpolatePuBuGn)
-      .domain(extent as [number, number]);
+  const rows = await d3.csv(
+    require("us_foreign_aid_country_deduped.csv"),
+    parseAidRecord
+  );
 
-    const getSpendingInRegion = (countryId: string): number | undefined => {
-      const region = latestYear.find(row => {
-        const country = ISO3166.whereAlpha3(row.countryCode);
-        return country ? country.numeric === countryId : false;
-      });
-      return region && region.currentAmount;
-    };
+  const latestYear = rows.filter(
+    row =>
+      row.fiscalYear.getFullYear() === 2017 &&
+      row.countryName !== "World" &&
+      row.transactionTypeName === "Obligations"
+  );
+  const extent = d3.extent(latestYear, row =>
+    row.currentAmount >= 1 ? row.currentAmount : undefined
+  );
+  const colourScale = d3
+    // @ts-ignore
+    .scaleSequentialLog(d3.interpolatePuBuGn)
+    .domain(extent as [number, number]);
 
-    svg
-      .append("g")
-      .selectAll("path")
-      .data(features)
-      .join("path")
-      .attr("fill", d => {
-        const spending = getSpendingInRegion(String(d.id));
-        return spending ? colourScale(spending) : "Silver";
-      })
-      .attr("d", path)
-      .append("title")
-      .text(d => {
-        const countryID = String(d.id);
-        const country = ISO3166.whereNumeric(countryID);
-        const countryString = country ? `${country.country} - ` : "";
-        const spending = getSpendingInRegion(countryID);
-        const spendingString = spending
-          ? `$${spending.toLocaleString()}`
-          : "N/A";
+  const getSpendingInRegion = (countryId: string): number | undefined => {
+    const region = latestYear.find(row => {
+      const country = ISO3166.whereAlpha3(row.countryCode);
+      return country ? country.numeric === countryId : false;
+    });
+    return region && region.currentAmount;
+  };
 
-        return countryString + spendingString;
-      });
+  svg
+    .append("g")
+    .selectAll("path")
+    .data(features)
+    .join("path")
+    .attr("fill", d => {
+      const spending = getSpendingInRegion(String(d.id));
+      return spending ? colourScale(spending) : "Silver";
+    })
+    .attr("d", path)
+    .append("title")
+    .text(d => {
+      const countryID = String(d.id);
+      const country = ISO3166.whereNumeric(countryID);
+      const countryString = country ? `${country.country} - ` : "";
+      const spending = getSpendingInRegion(countryID);
+      const spendingString = spending ? `$${spending.toLocaleString()}` : "N/A";
 
-    svg
-      .append("path")
-      .datum(topojson.mesh(world as any)!)
-      .attr("fill", "none")
-      .attr("stroke", "black")
-      .attr("stroke-linejoin", "round")
-      .attr("d", path);
-  }
-);
+      return countryString + spendingString;
+    });
+
+  svg
+    .append("path")
+    .datum(topojson.mesh(world as any)!)
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-linejoin", "round")
+    .attr("d", path);
+})();
