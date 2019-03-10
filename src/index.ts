7b1686b9a344ae3015051046bf8d41f19bca9ad2
Ivo Murrell <ivoisbelongtous@gmail.com>
2019-03-10 20:30:31 +0000

    Add US trade data to countries.
    Data sourced from the US census and ISO codes mapped by a helpful TXT
    from a hidden-away schedule file I rewrote as JSON.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as topojson from "topojson";
 import { GeometryCollection } from "topojson-specification";
 import world from "world-atlas/world/110m.json";
 
-import { parseAidRecord } from "parse";
+import { parseAidRecord, parseTradeRecord } from "parse";
 
 (async () => {
   ISOCountries.registerLocale(i18nCountry);
@@ -26,30 +26,48 @@ import { parseAidRecord } from "parse";
 
   const path = d3.geoPath(d3.geoEqualEarth());
 
-  const rows = await d3.csv(
+  const aidRows = await d3.csv(
     require("us_foreign_aid_country_deduped.csv"),
     parseAidRecord
   );
+  const tradeRows = await d3.csv(
+    require("us_trade_by_country.csv"),
+    parseTradeRecord
+  );
 
-  const latestYear = rows.filter(
+  const year = 2017;
+
+  const aidYear = aidRows.filter(
     row =>
-      row.fiscalYear.getFullYear() === 2017 &&
+      row.fiscalYear.getFullYear() === year &&
       row.countryName !== "World" &&
       row.transactionTypeName === "Obligations"
   );
-  const extent = d3.extent(latestYear, row =>
+  const tradeYear = tradeRows.filter(row => row.year.getFullYear() === year);
+
+  const aidExtent = d3.extent(aidYear, row =>
     row.currentAmount >= 1 ? row.currentAmount : undefined
   );
-  const colourScale = d3
+  const aidColourScale = d3
     // @ts-ignore
     .scaleSequentialLog(d3.interpolatePuBuGn)
-    .domain(extent as [number, number]);
+    .domain(aidExtent as [number, number]);
 
   const getSpendingInRegion = (countryId: number): number | undefined => {
-    const region = latestYear.find(
+    const region = aidYear.find(
       row => Number(ISOCountries.alpha3ToNumeric(row.countryCode)) === countryId
     );
     return region && region.currentAmount;
+  };
+
+  const getTradingInRegion = (
+    countryId: number
+  ): [number, number] | undefined => {
+    const region = tradeYear.find(
+      row =>
+        Number(ISOCountries.alpha2ToNumeric(row.countryISOCode)) === countryId
+    );
+    return region && [region.imports.total, region.exports.total];
   };
 
   svg
@@ -59,18 +77,25 @@ import { parseAidRecord } from "parse";
     .join("path")
     .attr("fill", d => {
       const spending = getSpendingInRegion(Number(d.id));
-      return spending ? colourScale(spending) : "Silver";
+      return spending ? aidColourScale(spending) : "Silver";
     })
     .attr("d", path)
     .append("title")
     .text(d => {
       const countryID = Number(d.id);
+
       const country = ISOCountries.getName(countryID, "en");
       const countryString = country ? `${country} - ` : "";
+
       const spending = getSpendingInRegion(countryID);
       const spendingString = spending ? `$${spending.toLocaleString()}` : "N/A";
 
-      return countryString + spendingString;
+      const trading = getTradingInRegion(countryID);
+      const tradingString = trading
+        ? `\n$${trading[0].toLocaleString()}/$${trading[1].toLocaleString()}`
+        : "";
+
+      return countryString + spendingString + tradingString;
     });
 
   svg
--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,7 @@
 import { DSVRowString } from "d3";
 
+import tradeCodeMap from "schedule_c_country_list_ISO_code.json";
+
 export interface AidRecord {
   countryCode: string;
   countryName: string;
@@ -41,5 +43,100 @@ export const parseAidRecord = (
     fiscalYear: new Date(rawRow.fiscal_year!),
     currentAmount: +rawRow.current_amount!,
     constantAmount: +rawRow.constant_amount!
+  };
+};
+
+interface MonthlyRecords {
+  january: number;
+  february: number;
+  march: number;
+  april: number;
+  may: number;
+  june: number;
+  july: number;
+  august: number;
+  september: number;
+  october: number;
+  november: number;
+  december: number;
+  total: number;
+}
+export interface TradeRecord {
+  year: Date;
+  countryCode: string;
+  countryISOCode: string;
+  countryName: string;
+  imports: MonthlyRecords;
+  exports: MonthlyRecords;
+}
+export type TradeRecordColumns =
+  | "year"
+  | "CTY_CODE"
+  | "CTYNAME"
+  | "IJAN"
+  | "IFEB"
+  | "IMAR"
+  | "IAPR"
+  | "IMAY"
+  | "IJUN"
+  | "IJUL"
+  | "IAUG"
+  | "ISEP"
+  | "IOCT"
+  | "INOV"
+  | "IDEC"
+  | "IYR"
+  | "EJAN"
+  | "EFEB"
+  | "EMAR"
+  | "EAPR"
+  | "EMAY"
+  | "EJUN"
+  | "EJUL"
+  | "EAUG"
+  | "ESEP"
+  | "EOCT"
+  | "ENOV"
+  | "EDEC"
+  | "EYR";
+
+export const parseTradeRecord = (
+  rawRow: DSVRowString<TradeRecordColumns>
+): TradeRecord => {
+  return {
+    year: new Date(rawRow.year!),
+    countryCode: rawRow.CTY_CODE!,
+    countryISOCode: (tradeCodeMap as any)[rawRow.CTY_CODE!],
+    countryName: rawRow.CTYNAME!,
+    imports: {
+      january: +rawRow.IJAN!,
+      february: +rawRow.IFEB!,
+      march: +rawRow.IMAR!,
+      april: +rawRow.IAPR!,
+      may: +rawRow.IMAY!,
+      june: +rawRow.IJUN!,
+      july: +rawRow.IJUL!,
+      august: +rawRow.IAUG!,
+      september: +rawRow.ISEP!,
+      october: +rawRow.IOCT!,
+      november: +rawRow.INOV!,
+      december: +rawRow.IDEC!,
+      total: +rawRow.IYR!
+    },
+    exports: {
+      january: +rawRow.EJAN!,
+      february: +rawRow.EFEB!,
+      march: +rawRow.EMAR!,
+      april: +rawRow.EAPR!,
+      may: +rawRow.EMAY!,
+      june: +rawRow.EJUN!,
+      july: +rawRow.EJUL!,
+      august: +rawRow.EAUG!,
+      september: +rawRow.ESEP!,
+      october: +rawRow.EOCT!,
+      november: +rawRow.ENOV!,
+      december: +rawRow.EDEC!,
+      total: +rawRow.EYR!
+    }
   };
 };
